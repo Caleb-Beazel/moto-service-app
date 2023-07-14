3b3b359c4aaf113db82e416949f4fb0e168877c6
Caleb Beazel <caleb.beazel@gmail.com>
2023-07-14 16:38:48 -0600

    login endpoint added. flask_login added, but not incorporated well.

--- crud.py
+++ crud.py
@@ -4,6 +4,18 @@ def create_user(username, email, password):
     user = User(username=username, email=email, password=password)
     return user
 
+def get_users():
+
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    
+    return User.query.get(user_id)
+
+def get_user_by_username(username):
+    
+    return User.query.filter(User.username == username).first() 
+
 def create_vehicle(user_id, vin, make, model, year, use_val, use_unit, vehicle_notes = None, vehicle_image_link = None):
     vehicle = Vehicle(user_id=user_id, vin=vin, make=make, model=model, year=year, use_val=use_val, use_unit=use_unit, vehicle_notes=vehicle_notes, vehicle_image_link=vehicle_image_link)
     return vehicle
--- server.py
+++ server.py
@@ -2,14 +2,35 @@ from flask import Flask, render_template, request, flash, session, redirect
 from model import connect_to_db, db
 from jinja2 import StrictUndefined
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_required
 
+import crud
 
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+@app.route("/")
+@login_required
+def homepage():
+    return render_template("homepage.html")
 
+@app.route("/login", methods=["POST"])
+def process_login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_username(username)
+
+    if not user or user.password != password:
+        flash("Either the username you provided does not exist or the password was entered incorrectly.")
+
+    else:
+        session["username"] = user.username
+        flash(f"Welcome back, {user.username}!")
 
 if __name__ == "__main__":
     connect_to_db(app)
